d4ec9859191bcb12c3fa006139af4e78d08e2bb8
Borad111 <meetpatel39501010@gmail.com>
2025-05-22 18:20:43 +0530

    Task 2
    Complete Hero and card

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/Components/Home/Navbar/Navbar";
+import MobileNav from "@/Components/Home/Navbar/MobileNav";
+import Responsive from "@/Components/Home/Navbar/Responsive";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,9 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Responsive/>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
