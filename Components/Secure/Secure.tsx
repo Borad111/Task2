7ce7bac143996a518c3a733d68423bd9588690b3
Borad111 <meetpatel39501010@gmail.com>
2025-05-23 10:31:02 +0530

    Complete in Office

--- Components/Secure/Secure.tsx
+++ Components/Secure/Secure.tsx
@@ -5,6 +5,7 @@ function Secure() {
     <div className='py-24 w-[80%] mx-auto'>
       <div className='text-center'>
         <h1>GenAi Data Protection</h1>
+        <h1 className='font-black text-3xl pt-4'>Secure your LLM workflow  before <br /> prompt leakage happens</h1>
       </div>
     </div>
   )
